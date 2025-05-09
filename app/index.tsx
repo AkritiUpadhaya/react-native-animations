6c94e49d0d3aaaf09ba64fa14409ecaef8ce4c34
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-05-09 22:39:02 +0545

    added predefined functions

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,36 @@
 import { Stack } from 'expo-router';
 
 import { View, Button } from 'react-native';
-import Animated, { useSharedValue } from 'react-native-reanimated';
+import Animated, { useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 
 export default function Home() {
   const width= useSharedValue(100)
   const height= useSharedValue(100)
+  const backgroundColor= useSharedValue('blue')
 
   const Animation= () => {
-    height.value= Math.random()*300
-    width.value= Math.random()*300
+
+    const randomHeight= Math.random()*300
+    const randomWidth= Math.random()*300
+    const randomColor= '#'+Math.floor(Math.random()*16777215).toString(16)
+
+    width.value= withTiming(randomWidth)
+    height.value= withSpring(randomHeight)
+    backgroundColor.value= withTiming(randomColor, {duration:2000})
   }
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
       <View>
+      <Button title="Animate" onPress={Animation}/>
         <Animated.View 
         style={{
             width: width,
             height: height,
-            backgroundColor: 'red',
+            backgroundColor: backgroundColor,
         }}
         ></Animated.View>
-        <Button title="Animate" onPress={Animation}/>
+        
       </View>
     </>
   );
